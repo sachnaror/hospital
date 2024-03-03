8d02546e208945c5bf9069c9d1a6edc047f01116
Sachin <sachnaror@gmail.com>
2024-03-03 19:27:17 +0530

    typing commit message is most boring part

--- hospital/wsgi.py
+++ hospital/wsgi.py
@@ -1,8 +1,8 @@
 
-from django.core.management import execute_from_command_line
 import os
 import sys
 
+from django.core.management import execute_from_command_line
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital.settings')
