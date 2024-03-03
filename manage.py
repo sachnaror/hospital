7ffe537d8bfc17995e458c66ede039c26abe7028
Sachin <sachnaror@gmail.com>
2024-03-03 20:07:05 +0530

    typing commit message is most boring part

--- manage.py
+++ manage.py
@@ -3,23 +3,20 @@
 import os
 import sys
 
-from django.apps import AppConfig
-from django.core.management import \
-    execute_from_command_line  # Add missing import statement
+from django.core.management import execute_from_command_line
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital.settings')
     try:
-        from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
