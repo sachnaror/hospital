69af3dfdf489aa8002e3cb8ccc9f14c0a5a175ad
Sachin <sachnaror@gmail.com>
2024-03-04 15:03:06 +0530

    typing commit message is most boring part

--- static/profile_pics/js/csr.js
+++
@@ -1,9 +0,0 @@
-import Cookies from 'js-cookie';
-
-$.ajaxSetup({
-    beforeSend: function (xhr, _settings) {
-        if (!this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
-        }
-    }
-});
